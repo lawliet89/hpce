3a23378bf981f6a83521c4de2ff7be20b6b60aca
Yong Wen Chua <yongwen.chua10@imperial.ac.uk>
2014-02-05 17:05:58 +0000

    Faster passthrough

--- hpce2/audio/passthrough.c
+++ hpce2/audio/passthrough.c
@@ -4,16 +4,25 @@
 #include <errno.h>
 #include <string.h>
 #include <stdint.h>
-
 #include <unistd.h>
 
+#define DEFAULT_SAMPLES 512
+
 int main(int argc, char *argv[])
-{	
+{
+	int n = DEFAULT_SAMPLES; // number of samples
+	// Buffer containing n sample (left and right, both 16 bit).
+	int16_t *samples;
+	unsigned cbBuffer;
+
+	if (argc > 1 ) {
+		n = atoi(argv[1]);
+	}
+
+	samples = malloc(sizeof(int16_t) * n);
+	cbBuffer=sizeof(sizeof(int16_t) * n);
+
 	while(1){
-		// Buffer containing one sample (left and right, both 16 bit).
-		int16_t samples[2];
-		unsigned cbBuffer=sizeof(samples);	// size in bytes of 
-		
 		// Read one sample from input
 		int got=read(STDIN_FILENO, samples, cbBuffer);
 		if(got<0){
@@ -21,13 +30,10 @@ int main(int argc, char *argv[])
 			exit(1);
 		}else if(got==0){
 			break;	 // end of file
-		}else if(got!=cbBuffer){
-			fprintf(stderr, "%s : Did not receive expected number of bytes.\n", argv[0]);
-			exit(1);
 		}
-		
-		// Copy one sample to output
-		int done=write(STDOUT_FILENO, samples, cbBuffer);
+
+		// Copy samples to output
+		int done=write(STDOUT_FILENO, samples, got);
 		if(done<0){
 			fprintf(stderr, "%s : Write to stdout failed, error=%s.", argv[0], strerror(errno));
 			exit(1);
@@ -36,5 +42,6 @@ int main(int argc, char *argv[])
 		}
 	}
 
+	free(samples);
 	return 0;
 }
