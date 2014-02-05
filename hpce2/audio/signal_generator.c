3c340c25a661fe2ac729841415ea705e08e11fd7
Yong Wen Chua <yongwen.chua10@imperial.ac.uk>
2014-02-05 17:42:03 +0000

    Signal Generator

---
+++ hpce2/audio/signal_generator.c
@@ -0,0 +1,54 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#define SAMPLE_SIZE 512
+#define DEFAULT_SINE_FREQ 50
+#define AMPLITUDE 30000
+#define SAMPLE_RATE 44100
+#define SAMPLE_INTERVAL 1.0/SAMPLE_RATE
+
+// 2 PI phase split over
+
+int main(int argc, char *argv[])
+{
+	int freq = DEFAULT_SINE_FREQ;
+	// Buffer containing n sample (left and right, both 16 bit).
+	int16_t samples[SAMPLE_SIZE];
+	unsigned cbBuffer = sizeof(int16_t) * SAMPLE_SIZE;
+	int i;
+	double phase = 0, t = 0;
+
+	if (argc > 1 ) {
+		freq = atoi(argv[1]);
+	}
+
+	while(1){
+		// Generate samples
+		for (i = 0; i < SAMPLE_SIZE; ++i) {
+			t += SAMPLE_INTERVAL;
+			phase += 2*M_PI*freq*SAMPLE_INTERVAL;
+
+			if (phase > 2*M_PI) {
+				phase = fmod(phase, 2*M_PI);
+				t = fmod(t, 1.0/freq);
+			}
+
+			samples[i] = (int16_t) (AMPLITUDE * sin(phase));
+		}
+		// Copy samples to output
+		int done=write(STDOUT_FILENO, samples, cbBuffer);
+		if(done<0){
+			fprintf(stderr, "%s : Write to stdout failed, error=%s.", argv[0], strerror(errno));
+			exit(1);
+		}else if(done!=cbBuffer){
+			fprintf(stderr, "%s : Could not read requested number of bytes from stream.\n", argv[0]);
+		}
+	}
+
+	return 0;
+}
