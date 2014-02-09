91ed88f1c9190eb64cf832d4fc637e2fec3a67a7
Yong Wen Chua <lawliet89@gmail.com>
2014-02-09 13:57:07 +0000

    Add corrupter script

---
+++ hpce2/audio/corrupter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -eu
+
+tempdir=$(mktemp -d "/tmp/corrupt.XXXXXXXXXX") ||\
+	{ echo "Failed to create temp directory"; exit 1; }
+trap "rm -rf $tempdir" EXIT
+
+# 				OUT
+#		1				2
+#	3		4		5		STDIN
+# 500 600 800 1000 1200 1400
+
+pipes=()
+for i in {1..5}; do
+		mkfifo "$tempdir/pipe$i"
+done
+
+./merge "$tempdir/pipe1" "$tempdir/pipe2" &\
+	./merge "$tempdir/pipe3" "$tempdir/pipe4" > "$tempdir/pipe1" &\
+	./merge "$tempdir/pipe5" <(cat -) > "$tempdir/pipe2" &\
+	./merge <(./signal_generator 500) <(./signal_generator 600) > "$tempdir/pipe3" &\
+	./merge <(./signal_generator 800) <(./signal_generator 1000) > "$tempdir/pipe4" &\
+	./merge <(./signal_generator 1200) <(./signal_generator 1400) > "$tempdir/pipe5"
