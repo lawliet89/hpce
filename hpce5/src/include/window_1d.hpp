e42b68e8c228921278c45dd313a9f7ab5950f0cd
Ryan <ryansavitski@gmail.com>
2014-03-05 15:17:03 +0000

    height passed to window func

--- hpce5/src/include/window_1d.hpp
+++ hpce5/src/include/window_1d.hpp
@@ -15,7 +15,10 @@ struct win_queue_entry
 //TODO: out_buf
 //TODO: remove bitwidth via specialisation
 //TODO: condition var+mutex argument?
-void window_1d(uint8_t* const in_buf, uint8_t* const out_buf, uint64_t buf_size, const uint32_t chunk_size, const uint32_t img_width_pix, const uint32_t n_levels, const uint8_t bit_width);
+void window_1d(uint8_t* const in_buf, uint8_t* const out_buf, uint64_t buf_size,
+               const uint32_t chunk_size, const uint32_t img_width_pix,
+               const uint32_t img_height_pix, const uint32_t n_levels,
+               const uint8_t bit_width);
 // chunk size in bytes
 
 
---
+++ hpce5/src/window_1d.cpp
@@ -0,0 +1,11 @@
+// does 1d rolling window min/max over the given chunks of rows and accumulates
+// results vertically
+void window_1d(uint8_t* const in_buf, uint8_t* const out_buf, uint64_t buf_size,
+               const uint32_t chunk_size, const uint32_t img_width_pix,
+               const uint32_t img_height_pix, const uint32_t n_levels,
+               const uint8_t bit_width)
+{
+  // fix up img width and chunk size and whatnot
+
+  return;
+}
