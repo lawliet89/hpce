834b623d22c433bbb7e4af9b0b7f7d104c150608
Ryan <ryansavitski@gmail.com>
2014-03-07 23:19:38 +0000

    cleanup

--- hpce5/src/window_1d.cpp
+++ hpce5/src/window_1d.cpp
@@ -1,12 +1,10 @@
 #include "include/window_1d.hpp"
 #include <algorithm>
 #include <cstdint>
-
-// TODO
-#include <unistd.h>
-#include <stdio.h>
 #include <mutex>
 
+namespace
+{
 struct win_queue_entry
 {
   uint32_t value;
@@ -20,14 +18,9 @@ struct window_state
   win_queue_entry* q_head;
   win_queue_entry* q_tail;
 };
+}
 
-// TODO: functions for output since it's copypasted three times
-
-// TODO fri:
-
-// output func [&] or inline directly, per-pixel, advances chunk ptr as required
-// (only forward in chunk sizes)
-
+// TODO
 // make inner loop operate on individual pixels?
 // should be fixable +- okish for multibyte
 // reading accums and such will still need repacking individual pixels...
@@ -40,8 +33,7 @@ void window_1d_min(uint8_t* const in_buf, uint8_t* const out_buf,
                    const uint32_t n_levels, const uint8_t bit_width,
                    ReadWriteSync& producer, ReadWriteSync& consumer)
 {
-  // TODO: in threaded case, local vals, not static
-  uint32_t i;  // counts bytes
+  uint32_t i;  // counts bytes //TODO
 
   uint32_t img_w_bytes;
   uint64_t extra_chunks;
@@ -57,46 +49,40 @@ void window_1d_min(uint8_t* const in_buf, uint8_t* const out_buf,
   window_state* wss;
   window_state* ws;
 
-  // TODO: threaded -> done once anyway
-  bool serial_hack = 0;
-
+  // TODO: move below initialisation?
   std::unique_lock<std::mutex> lock = consumer.producerWait();
 
-  if (!serial_hack) {
-    //////////////////////////
-    // init here
-    //////////////////////////
-    i = 0;
-    img_w_bytes = (img_width_pix * 8) / bit_width;
-    uint64_t chunks_per_img =
-        ((img_height * img_w_bytes) + chunk_size - 1) / chunk_size;
-    uint64_t chunks_per_img_padded =
-        (((img_height + n_levels) * img_w_bytes) + chunk_size - 1) / chunk_size;
-    extra_chunks = chunks_per_img_padded - chunks_per_img;
-    row_cnt = 0;
-    curr_chunk = in_buf;
+  // initialise counters and compute chunk padding for extra N rows
+  i = 0;
 
-    out_subchunk_cnt = 0;
-    curr_out_chunk = out_buf;
+  img_w_bytes = (img_width_pix * 8) / bit_width;
+  uint64_t chunks_per_img =
+      ((img_height * img_w_bytes) + chunk_size - 1) / chunk_size;
+  uint64_t chunks_per_img_padded =
+      (((img_height + n_levels) * img_w_bytes) + chunk_size - 1) / chunk_size;
+  extra_chunks = chunks_per_img_padded - chunks_per_img;
 
-    // create and initialise rolling windows
-    num_windows_assigned = n_levels;
-    wss = new window_state[num_windows_assigned];
-    for (int m = 0; m < num_windows_assigned; ++m) {
-      window_state* ws = &wss[m];
-      ws->window_size = (m + 1) * 2 + 1;
-      ws->start = new win_queue_entry[ws->window_size];
-      ws->q_head = ws->start;
+  row_cnt = 0;
+  curr_chunk = in_buf;
 
-      ws->q_tail = ws->q_head;
-      ws->q_head->value = 0xffffffff;  // TODO: max different
-      ws->q_head->retire_idx = ws->window_size;
-    }
+  out_subchunk_cnt = 0;
+  curr_out_chunk = out_buf;
 
-    //////////////////////////
-    serial_hack = 1;
+  // create and initialise rolling windows
+  num_windows_assigned = n_levels;
+  wss = new window_state[num_windows_assigned];
+  for (int m = 0; m < num_windows_assigned; ++m) {
+    window_state* ws = &wss[m];
+    ws->window_size = (m + 1) * 2 + 1;
+    ws->start = new win_queue_entry[ws->window_size];
+    ws->q_head = ws->start;
+
+    ws->q_tail = ws->q_head;
+    ws->q_head->value = 0xffffffff;  // TODO: max different
+    ws->q_head->retire_idx = ws->window_size;
   }
 
+  // main thread loop
   while (1) {
     producer.consumerWait();
 
@@ -108,39 +94,30 @@ void window_1d_min(uint8_t* const in_buf, uint8_t* const out_buf,
     producer.consume();
     producer.hintProducer();
 
-    // TODO: flow control with producer here
     // iterate through chunk
     for (int j = 0; j < chunk_size; ++j) {  // TODO multibyte
-
       uint8_t curr_val = *(curr_chunk + j);
 
-      // TODO
+      // output vertical accumulator now that the bottom pixel of the unrolled
+      // diamond is available
       if (row_cnt >= n_levels) {
         uint8_t* acc0 = curr_chunk + j - (2 * n_levels) * img_w_bytes;
         acc0 += (acc0 < in_buf ? buf_size : 0);
 
-        // TODO
-        //      uint8_t temp = std::min(*acc0, curr_val);
-
         *(curr_out_chunk + out_subchunk_cnt) = std::min(*acc0, curr_val);
         if (out_subchunk_cnt++ == chunk_size - 1) {
-          // TODO: SYNC: .produce() followed by .wait
-          // TODO: sync for the starting chunk here
-          // write(STDOUT_FILENO, curr_out_chunk, chunk_size);
           consumer.produce(std::move(lock));
           lock = consumer.producerWait();
-          curr_out_chunk += chunk_size;
-          curr_out_chunk -=
-              (curr_out_chunk >= out_buf + buf_size) ? buf_size : 0;
+
+          curr_out_chunk = (curr_out_chunk + chunk_size == out_buf + buf_size)
+                               ? out_buf
+                               : curr_out_chunk + chunk_size;
 
           out_subchunk_cnt = 0;
         }
-        // fprintf(stderr, "OUT: %2x\n", std::min(*acc0, curr_val));
-        // write(STDOUT_FILENO, &temp, 1);
       }
-      // TODO
 
-      // step all windows by 1
+      // step all windows by 1 pixel
       for (int m = 0; m < num_windows_assigned; ++m) {
         ws = &wss[m];
 
@@ -204,60 +181,51 @@ void window_1d_min(uint8_t* const in_buf, uint8_t* const out_buf,
       }
 
       // if at the start of a row, restart the windows' state
-      if (i++ == img_w_bytes - 1) {  // TODO: preincrement shouldn't break it
-                                     // and will look neater
+      if (++i == img_w_bytes) {  // TODO: preincrement shouldn't break it
         i = 0;
         if (++row_cnt == img_height) {
-
-          // finish current chunk
+          // finish current chunk with accumulator drain (no window updates)
           while (++j < chunk_size) {
             uint8_t* acc0 = curr_chunk + j - (2 * n_levels) * img_w_bytes;
             acc0 += (acc0 < in_buf ? buf_size : 0);
 
-            // fprintf(stderr, "OUT FIRSTDRAIN: %2x\n", *acc0);  // TODO: output
-            // here
-            //    write(STDOUT_FILENO, acc0, 1);
-
             *(curr_out_chunk + out_subchunk_cnt) = *acc0;
             if (out_subchunk_cnt++ == chunk_size - 1) {
-              // TODO: SYNC: .produce() followed by .wait
-              // TODO: sync for the starting chunk here
+              // signal production of chunk and wait for a consumption of the
+              // previous chunk
               consumer.produce(std::move(lock));
               lock = consumer.producerWait();
-              // write(STDOUT_FILENO, curr_out_chunk, chunk_size);
 
-              curr_out_chunk += chunk_size;
-              curr_out_chunk -=
-                  (curr_out_chunk >= out_buf + buf_size) ? buf_size : 0;
+              curr_out_chunk =
+                  (curr_out_chunk + chunk_size == out_buf + buf_size)
+                      ? out_buf
+                      : curr_out_chunk + chunk_size;
 
               out_subchunk_cnt = 0;
             }
-            // fprintf(stderr, "OUT: %2x\n", std::min(*acc0, curr_val));
           }
           curr_chunk = (curr_chunk + chunk_size == in_buf + buf_size)
                            ? in_buf
                            : curr_chunk + chunk_size;
 
-          // for any extra chunks: do them (with flow control!)
+          // flush any extra accumulators for extra N rows at the bottom of the
+          // image
           for (uint64_t p = 0; p < extra_chunks; ++p) {
-            // TODO: flow control for output  here!
             for (uint32_t j = 0; j < chunk_size; ++j) {
               uint8_t* acc0 = curr_chunk + j - (2 * n_levels) * img_w_bytes;
               acc0 += (acc0 < in_buf ? buf_size : 0);
 
-              // fprintf(stderr, "OUT: %2x\n", *acc0);  // TODO: output here
-              //  write(STDOUT_FILENO, acc0, 1);
               *(curr_out_chunk + out_subchunk_cnt) = *acc0;
-              if (out_subchunk_cnt++ == chunk_size - 1) {
-                // TODO: SYNC: .produce() followed by .wait
-                // TODO: sync for the starting chunk here
+              if (++out_subchunk_cnt == chunk_size) {
+                // signal production of chunk and wait for a consumption of the
+                // previous chunk
                 consumer.produce(std::move(lock));
                 lock = consumer.producerWait();
-                // write(STDOUT_FILENO, curr_out_chunk, chunk_size);
 
-                curr_out_chunk += chunk_size;
-                curr_out_chunk -=
-                    (curr_out_chunk >= out_buf + buf_size) ? buf_size : 0;
+                curr_out_chunk =
+                    (curr_out_chunk + chunk_size == out_buf + buf_size)
+                        ? out_buf
+                        : curr_out_chunk + chunk_size;
 
                 out_subchunk_cnt = 0;
               }
@@ -265,18 +233,16 @@ void window_1d_min(uint8_t* const in_buf, uint8_t* const out_buf,
             curr_chunk = (curr_chunk + chunk_size == in_buf + buf_size)
                              ? in_buf
                              : curr_chunk + chunk_size;
-
-            // fprintf(stderr, "\nextra chunk done!\n");
           }
 
           // TODO
-          // fprintf(stderr, "\nDONE!\n");
+          // synchronise reset
           std::unique_lock<std::mutex> resetLock = consumer.waitForReset();
 
           // reset
           consumer.resetDone(std::move(resetLock));
           producer.signalReset();
-          // return;
+          // TODO once: free memory at thread cleanup
         }
       }
     }
@@ -284,11 +250,13 @@ void window_1d_min(uint8_t* const in_buf, uint8_t* const out_buf,
     curr_chunk = (curr_chunk + chunk_size == in_buf + buf_size)
                      ? in_buf
                      : curr_chunk + chunk_size;
-    // TODO: same for out_buf
-
-    // TODO once: free memory at thread cleanup
   }
 }
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 // does 1d rolling window min/max over the given chunks of rows and accumulates
 // results vertically
