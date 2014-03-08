ce9296f77e2850fa88eb7771591093a86b5085e4
Ryan <ryansavitski@gmail.com>
2014-03-08 21:46:11 +0000

    prep for minmax templating

--- hpce5/src/window_1d.cpp
+++ hpce5/src/window_1d.cpp
@@ -38,10 +38,9 @@ void window_1d_min(uint8_t* const in_buf, uint8_t* const out_buf,
 {
   try
   {
-    uint32_t i;  // counts bytes //TODO
+    uint32_t i;  // counts pixels per row
 
     uint32_t img_w_bytes;
-    uint64_t extra_chunks;
 
     uint32_t row_cnt;
     uint8_t* curr_chunk;  // TODO: multibyte version
@@ -55,24 +54,22 @@ void window_1d_min(uint8_t* const in_buf, uint8_t* const out_buf,
     window_state* ws;
 
     std::unique_lock<std::mutex> lock;
-
     std::unique_lock<std::mutex> resetLock = consumer.waitForReset();
 
     // initialise counters and compute chunk padding for extra N rows
     i = 0;
 
     img_w_bytes = (img_width_pix * 8) / bit_width;
-    uint64_t chunks_per_img =
-        ((img_height * img_w_bytes) + chunk_size - 1) / chunk_size;
-    uint64_t chunks_per_img_padded =
-        (((img_height + n_levels) * img_w_bytes) + chunk_size - 1) / chunk_size;
-    extra_chunks = chunks_per_img_padded - chunks_per_img;
 
     row_cnt = 0;
     curr_chunk = in_buf;
 
     out_subchunk_cnt = 0;
     curr_out_chunk = out_buf;
+
+    auto le_ge_cmp = [](uint8_t &lhs, uint32_t &rhs){return lhs <= rhs;};
+    auto min_or_max = (const uint8_t&(*)(const uint8_t&, const uint8_t&)) &std::min;
+    uint32_t q_init_val = 0xffffffff;
 
     // create and initialise rolling windows
     num_windows_assigned = n_levels;
@@ -84,9 +81,10 @@ void window_1d_min(uint8_t* const in_buf, uint8_t* const out_buf,
       ws->q_head = ws->start;
 
       ws->q_tail = ws->q_head;
-      ws->q_head->value = 0xffffffff;  // TODO: max different
+      ws->q_head->value = q_init_val;
       ws->q_head->retire_idx = ws->window_size;
     }
+
 
     // cleanup to be done before terminating thread
     auto clean_memory = [&wss, &num_windows_assigned]() {
@@ -148,7 +146,7 @@ void window_1d_min(uint8_t* const in_buf, uint8_t* const out_buf,
           uint8_t* acc0 = curr_chunk + j - (2 * n_levels) * img_w_bytes;
           acc0 += (acc0 < in_buf ? buf_size : 0);
 
-          output_synced(std::min(*acc0, curr_val));
+          output_synced(min_or_max(*acc0, curr_val));
         }
 
         // step all windows by 1 pixel
@@ -162,12 +160,14 @@ void window_1d_min(uint8_t* const in_buf, uint8_t* const out_buf,
             if (ws->q_head >= end)
               ws->q_head = ws->start;
           }
-          if (curr_val <= ws->q_head->value) {
+          //if (curr_val <= ws->q_head->value) {
+          if (le_ge_cmp(curr_val, ws->q_head->value)) {
             ws->q_head->value = curr_val;
             ws->q_head->retire_idx = i + ws->window_size;
             ws->q_tail = ws->q_head;
           } else {
-            while (ws->q_tail->value >= curr_val) {
+            while (le_ge_cmp(curr_val, ws->q_tail->value)) {
+            //while (curr_val <= ws->q_tail->value) {
               if (ws->q_tail == ws->start)
                 ws->q_tail = end;
               --(ws->q_tail);
@@ -188,11 +188,11 @@ void window_1d_min(uint8_t* const in_buf, uint8_t* const out_buf,
 
           // do not process windows that aren't centered within the image
           if (i >= n_depth) {
-            *acc1 = std::min(*acc1, (uint8_t)ws->q_head->value);
-            *acc2 = std::min(*acc2, (uint8_t)ws->q_head->value);
+            *acc1 = min_or_max(*acc1, (uint8_t)ws->q_head->value);
+            *acc2 = min_or_max(*acc2, (uint8_t)ws->q_head->value);
           }
           // special drain
-          if (i == img_w_bytes - 1) {
+          if (i == img_width_pix - 1) {
             for (uint32_t ii = 1; ii <= n_depth; ++ii) {
               if (ws->q_head->retire_idx == i + ii) {
                 ws->q_head++;
@@ -206,17 +206,17 @@ void window_1d_min(uint8_t* const in_buf, uint8_t* const out_buf,
                               (2 * n_levels - n_depth) * img_w_bytes - n_depth;
               acc2 += (acc2 < in_buf ? buf_size : 0);
 
-              *acc1 = std::min(*acc1, (uint8_t)ws->q_head->value);
-              *acc2 = std::min(*acc2, (uint8_t)ws->q_head->value);
+              *acc1 = min_or_max(*acc1, (uint8_t)ws->q_head->value);
+              *acc2 = min_or_max(*acc2, (uint8_t)ws->q_head->value);
             }
             ws->q_tail = ws->q_head;
-            ws->q_head->value = 0xffffffff;  // TODO: max different
+            ws->q_head->value = q_init_val;
             ws->q_head->retire_idx = ws->window_size;
           }
         }
 
         // if at the start of a row, restart the windows' state
-        if (++i == img_w_bytes) {
+        if (++i == img_width_pix) {
           i = 0;
           // if done with image input
           if (++row_cnt == img_height) {
